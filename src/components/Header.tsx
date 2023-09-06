f4d6b0e702f92fe5a48cdebf4ee0ad73f1920d13
Muhammad Awais <m.awais2030@gmail.com>
2023-09-06 18:58:51 +0500

    Header Done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,18 @@
+import { Button, HStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <HStack p={'4'} shadow={'base'} bgColor={'blackAlpha.700'}>
+      <Button variant={'unstyled'} color={'white'}>
+        <Link to={'/'}>Home</Link>
+      </Button>
+      <Button variant={'unstyled'} color={'white'}>
+        <Link to={'/exchanges'}>Exchanges</Link>
+      </Button>
+      <Button variant={'unstyled'} color={'white'}>
+        <Link to={'/coins'}>Coins</Link>
+      </Button>
+    </HStack>
+  )
 }
