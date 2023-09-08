1200d955758b15a01562e5dc50aa4054fe9d45ed
Muhammad Awais <m.awais2030@gmail.com>
2023-09-08 11:45:19 +0500

    Home Page Complete

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,25 @@
+import { Box, Image, Text } from '@chakra-ui/react'
+import btcSrc from '../assets/btc.png'
+
 export const Home = () => {
-  return <div>Home</div>
+  return (
+    <Box bgColor={'blackAlpha.700'} w={'full'} h={'85vh'}>
+      <Image
+        h={'full'}
+        w={'full'}
+        objectFit={'contain'}
+        src={btcSrc}
+        filter={'grayscale(0.8)'}
+      />
+      <Text
+        fontSize={'6xl'}
+        textAlign={'center'}
+        fontWeight={'thin'}
+        color={'whiteAlpha.500'}
+        mt={'-20'}
+      >
+        CryptoCO
+      </Text>
+    </Box>
+  )
 }
